3bc97b33e8162646995c32fbff85ed9c1f683e22
张旭东 <xudongzhang999@163.com>
2017-11-26 17:57:55 +0800

    添加Mantle Coreda VC

---
+++ OXCMantle/ModelExample/CoreDataMantle/OXCoreDataMantleVC.h
@@ -0,0 +1,13 @@
+//
+//  OXCoreDataMantleVC.h
+//  OXCMantle
+//
+//  Created by 张旭东 on 2017/11/26.
+//  Copyright © 2017年 张旭东. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OXCoreDataMantleVC : UIViewController
+
+@end
